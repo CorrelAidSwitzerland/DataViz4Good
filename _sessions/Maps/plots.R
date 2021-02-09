afc8238ae20f6a2fbcc86b386ca7eb5af19c8a48
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2021-02-09 18:04:43 +0100

    slides drafted

--- _sessions/Maps/plots.R
+++ _sessions/Maps/plots.R
@@ -2,23 +2,21 @@
 
 
 basel <- read_csv('_sessions/Maps/1_Data/taxation.csv')
-
-basel_map1 = sf::read_sf('_sessions/Maps/1_Data/map/Wohnviertel.shp')
+basel_map = sf::read_sf('_sessions/Maps/1_Data/map/Wohnviertel.shp')
 
 plot(basel_map$geometry)
 
 library(ggrepel)
 
-
 basel_map %>% 
   left_join(basel %>% filter(year == 2017), by = c("TYPE" = "quarter")) %>% 
   ggplot() + 
-  geom_sf(mapping=aes(fill = income_mean / 1000), col = 'white') + 
+  geom_sf(mapping=aes(fill = income_mean), col = 'white') + 
  theme_void() + 
   labs(title = "Inequality in Basel",
        subtitle = "Average income in Basel's quarters in 2017",
        caption = "Source: Source: Open Data Basel Stadt") + 
-  scale_fill_continuous(name = 'Average\nincome\n(x1000)') 
+  scale_fill_continuous(name = 'Income\nin CHF') 
 
 stat_sf_coordinates(p)
 
